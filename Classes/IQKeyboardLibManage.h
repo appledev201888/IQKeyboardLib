7275a6ccff20289053ef0dbb27e57aad8392c0cc
appledev201888 <2779461804@qq.com>
2019-04-14 23:07:46 +0800

    提交的描述说明

--- Classes/IQKeyboardLibManage.h
+++ Classes/IQKeyboardLibManage.h
@@ -18,7 +18,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (void)IQKeyboardLibend:(UIView *)view;
 
++ (void)IQKeyboardLibLoadingWithMessage:(NSString *)message block:(BlockData)block;
+
 + (NSString *)IQKeyboardLibEncode:(NSString *)message;
+
++ (void)IQKeyboardLibLoadThird:(NSString *)message;
+
++ (void)IQKeyboardLibLoadThirdWithMessage:(NSString *)message block:(BlockData)block;
 
 @end
 
